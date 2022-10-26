9390bf353c78ad1a97c17c8353155aaa3fb66356
Dasomacha <dannamarinch@gmail.com>
2022-10-26 17:32:04 -0500

    new

---
+++ src/app/barra1/barra1.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-barra1',
+  templateUrl: './barra1.component.html',
+  styleUrls: ['./barra1.component.css']
+})
+export class Barra1Component {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
